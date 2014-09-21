bc63d8e0799ba89cbf449c4e33f01767db50c814
Daniel Nichols <thebagmaster@gmail.com>
2014-09-20 22:02:16 -0500

    First Commit

---
+++ git.bat
@@ -0,0 +1,8 @@
+git.exe init
+git.exe add .
+git.exe commit -m "First Commit"
+git.exe remote add origin https://github.com/thebagmaster/AHK_EVE_BOT.git
+git.exe pull https://github.com/thebagmaster/AHK_EVE_BOT.git
+git.exe push origin master
+echo Press Enter...
+read
